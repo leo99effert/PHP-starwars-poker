9ddf92e5b4179d5207d6a99859bab4f5a14f8888
leo <leo99effert@outlook.com>
2023-06-06 16:54:53 +0200

    added normalize css file

--- sabacc.php
+++ sabacc.php
@@ -12,6 +12,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sabacc</title>
+    <link rel="stylesheet" type="text/css" href="normalize.css">
     <link rel="stylesheet" href="styles.css">
 </head>
 
