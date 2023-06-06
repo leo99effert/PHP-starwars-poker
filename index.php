5a408676c4ec733aa1df61064491755d0a9c49a4
leo <leo99effert@outlook.com>
2023-06-06 16:18:05 +0200

    added an introduction in the index file

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
-<?php 
-
-echo 'Welcome to the ';
-
-?>
-
+<?= 'Welcome to the '; ?>
 <a href="sabacc.php">table</a>
+<br>
+<br>
+<p>In Sabacc the goal is to get a score as close to 23 or -23 as possible without going over.</p>
+<p>It is also possible to get a hand that is even better than 23 and -23 called 'The Idiots Array'.</p>
+<p>You achieve it by having these cards in your hand: a zero, a two or minus two, and a three or minus three.</p>
+<p>You then spell 023 instead of getting it by adding. </p>
+<br>
+<p>Here is an example of some hands ranked:</p>
+<p>The Idiots Array (example 3,-2,0)</p>
+<p>23 (the second best hand possible)</p>
+<p>-23 (as good as positive 23)</p>
+<p>20</p>
+<p>-15</p>
+<p>12</p>
+<p>24 (to high)</p>
+<p>-26 (also to 'high')</p>
