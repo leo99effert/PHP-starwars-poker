5121e11cfbafdb78226200fdf3e5d1c911a43441
leo <leo99effert@outlook.com>
2023-06-08 20:43:46 +0200

    combined the play action with the sabacc shift

--- play.php
+++ play.php
@@ -8,6 +8,10 @@ $card = $_POST['card'];
 $index = array_search($card, $_SESSION['hand']);
 unset($_SESSION['hand'][$index]);
 $_SESSION['board'][] = $card;
+foreach ($_SESSION['hand'] as &$card) {
+    $card = rand(-13, 13);
+}
+$_SESSION['total'] = array_sum($_SESSION['hand']) + array_sum($_SESSION['board']);
 $_SESSION['round']++;
 header('location:sabacc.php');
 
